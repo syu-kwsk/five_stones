ebb53f353b0776c444d54b8f5ff1a77814383574
wata <39556764+syu-kwsk@users.noreply.github.com>
2018-11-13 00:19:04 +0900

    Add files via upload

---
+++ five_stones.cpp
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+#define NUMBER 8
+ int masume[NUMBER][NUMBER];
+  int tate;
+  int yoko;
+  int i;
+
+void make_gobann(){ for (i = 0; i < NUMBER; i++){
+    printf("%3d", i + 1);
+  }
+  
+  puts("\n  ________________________");
+
+  for (tate = 0;tate < NUMBER; tate++){
+    printf("%d |", tate + 1);
+    for(yoko = 0; yoko < NUMBER; yoko++){
+      masume[yoko][tate]  =  64;
+      if  (masume[yoko][tate] == 64){
+        printf("   ");
+      }
+    }
+    putchar('\n');
+  } //made gobann
+}
+int main(){
+    
+  make_gobann();
+ 
+ 
+  return 0;
+} 
+  
+    
+    
