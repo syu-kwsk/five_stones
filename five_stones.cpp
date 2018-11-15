287d95f49868acc93681b62d18616fc535526d12
syu-kwsk <kawasaki.wataru688@mail.kyutech.jp>
2018-11-15 19:54:15 +0900

    (x,y)に入力できるようにしました。

--- five_stones.cpp
+++ five_stones.cpp
@@ -1,34 +1,73 @@
 #include <stdio.h>
 
 #define NUMBER 8
- int masume[NUMBER][NUMBER];
-  int tate;
-  int yoko;
-  int i;
+int masume[NUMBER][NUMBER];
+int tate;
+int yoko;
+int i, j, k;
+int x = 0;
+int y = 0;
+int O; //white_stone
+int X; //black_stone
 
-void make_gobann(){ for (i = 0; i < NUMBER; i++){
-    printf("%3d", i + 1);
+void make_gobann(){
+
+  /*for (i = 0; i < NUMBER; i++){
+    printf(" %2d", i + 1);
   }
   
-  puts("\n  ________________________");
-
+  puts("\n  ---------------------------");
+  */
   for (tate = 0;tate < NUMBER; tate++){
-    printf("%d |", tate + 1);
+    /*printf("%d|", tate + 1);*/
     for(yoko = 0; yoko < NUMBER; yoko++){
       masume[yoko][tate]  =  64;
       if  (masume[yoko][tate] == 64){
-        printf("   ");
+        /* printf("   ");*/
       }
     }
-    putchar('\n');
+    /* putchar('\n');*/
   } //made gobann
 }
 int main(){
-    
-  make_gobann();//made gobann
- 
- 
-  return 0;
+  /*初期化*/
+  make_gobann();
+  
+  
+  /*入力*/
+
+  puts("decide your stone's position! \n");
+  printf("x:");scanf("%d", &j);
+  printf("y:");scanf("%d", &k);
+  x = j - 1;
+  y = k - 1;
+  masume[y][x] = O;
+
+  
+  /*出力*/
+  puts(" ");
+  for (i = 0; i < NUMBER; i++){
+    printf("%3d", i + 1);
+  }
+  puts("\n --------------------------");
+
+  for (tate = 0;tate < NUMBER; tate++){
+    printf("%d|", tate + 1);
+    for(yoko = 0; yoko < NUMBER; yoko++){
+      if  (masume[tate][yoko] == masume[y][x]){
+        printf("O  ");
+      }
+      else if (masume[tate][yoko] == 64){
+        printf("B  ");
+      }
+          
+    }
+  
+  putchar('\n');
+
+  }
+  
+return 0;
 } 
   
     
