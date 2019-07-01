9cef82071874d80dff727fa42f9e6e6e9621b9be
syu-kwsk <kawasaki.wataru688@mail.kyutech.jp>
2019-07-02 00:36:33 +0900

    Add stone class and set stone pos

--- src/five_stones/main.cpp
+++ src/five_stones/main.cpp
@@ -1,6 +1,29 @@
 #include <iostream>
 
-int main(){
 
+class stone{
+  int pos[2];
+  
+public:
+  void set_pos(int x, int y);
+
+
+};
+
+void stone::set_pos(int x, int y){
+  pos[0] = x;
+  pos[1] = y;
+
+}
+ 
+
+
+int main(){
+  stone ply1, ply2;
+  ply1.set_pos(5, 5);
+  ply2.set_pos(1, 1);
+  
+
+  
   return 0;
 }
