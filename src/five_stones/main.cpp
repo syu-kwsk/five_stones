55284517eab25336ea94bc3b6fb97e9c5150d54f
syu-kwsk <kawasaki.wataru688@mail.kyutech.jp>
2019-07-02 00:53:25 +0900

    Add get_pos to show stone pos

--- src/five_stones/main.cpp
+++ src/five_stones/main.cpp
@@ -1,11 +1,11 @@
 #include <iostream>
 
-
 class stone{
   int pos[2];
   
 public:
   void set_pos(int x, int y);
+  int get_pos(int pos_num);
 
 
 };
@@ -15,15 +15,20 @@ void stone::set_pos(int x, int y){
   pos[1] = y;
 
 }
- 
 
+int stone::get_pos(int pos_num){
+  int a = pos[pos_num];
+  return a;
+}
+ 
 
 int main(){
   stone ply1, ply2;
-  ply1.set_pos(5, 5);
+  ply1.set_pos(5, 4);
   ply2.set_pos(1, 1);
-  
 
+  std::cout << "ply1 x = " << ply1.get_pos(0) << std::endl;
+  std::cout << "ply1 y = " << ply1.get_pos(1) << std::endl;
   
   return 0;
 }
